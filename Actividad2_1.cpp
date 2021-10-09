b9893097eaad46c0cdf11ced0a72143c333075ea
AnYelg <73191386+AnYelg@users.noreply.github.com>
2021-10-08 22:53:09 -0500

    :)

--- Actividad2_1.cpp
+++ Actividad2_1.cpp
@@ -7,58 +7,48 @@ using namespace std;
 template <class T>
 class Nodo{
 	public:
-        T value;
-        Nodo *next;
-        Nodo *prev;
-
-        Nodo(T val)
-        {
-            value=val;
-            next=NULL;
-            prev=NULL;
-        }
+	T value;
+	Nodo *next;
+	
+	Nodo(T val){
+		value = val;
+		next = NULL;
+	}
 };
 
 template <class T>
 class LinkedList{
-    Nodo<T> *head;
-	Nodo<T> *tail;
 	public:
 		Nodo<T> *root;
 		
 		LinkedList(){
 			root=NULL;
-		    head=NULL;
-		    tail=NULL;
 		}
-		
-		
+
+        void append(Nodo<T> *nuevo){
+			if(root==NULL){
+				root=nuevo;
+			}else{
+				Nodo<T> *it=root;
+				while(it->next!=NULL){
+					it=it->next;
+				}
+				it->next=nuevo;
+			}
+		}
+        
 		void append(T val){
 			Nodo<T> *nuevo=new Nodo<T>(val);
 			this->append(nuevo);
 		}
 		
 		void imprimir(){
-			Nodo<T> *temp=head;
-            while(temp!=NULL){
-                cout<<temp->value<<",";
-                temp=temp->next;
-            }
-            cout<<endl;
-		}
-		
-		void accederElemento(int i){
-			int actual=0;
 			Nodo<T> *it=root;
-			while(it!=NULL && actual<i){
-				actual++;
+			while(it!=NULL){
+				cout<<it->value<<",";
 				it=it->next;
 			}
-			if(actual==i){
-				cout<<it->value<<endl;
-			}else{
-				cout<<"no existe esa posicion"<<endl;
-			}
+			cout<<endl;
 		}
 
         void Count(T i){
@@ -74,39 +64,76 @@ class LinkedList{
 			}
             cout<<"Tu número buscado: " <<i << " se encuentra: " << veces <<" veces en la lista" << endl;
 		}
-		
-		void modificarElemento(int i, T val){
-			int actual=0;
-			Nodo<T> *it=root;
-			while(it!=NULL && actual<i){
-				actual++;
-				it=it->next;
-			}
-			if(actual==i){
-				it->value=val;
-			}else{
-				cout<<"no existe esa posicion"<<endl;
-			}
-		}
+        
+		void DeleteList(){
+            Nodo<T> *temp=root;
+            Nodo<T> *it=root;
 
-        void append(Nodo<T> *nuevo){
-            if(head == NULL){
-                head = nuevo;
-                tail = head;
-                return;
+			if(root==NULL){
+				return;
+			}
+
+			while(it->next!=NULL){
+                Nodo<T> *temp=it->next;
+				it->next=temp->next;
+				delete temp;
+			}
+            
+			root=root->next;
+			delete temp;
+			return;
+		}
+        
+		/*void RemoveDuplicates(){ 
+            if(root==NULL){
+				return;
+			}
+
+            if(root->value == root->next->value){
+                Nodo<T> *temp=root;
+				root=root->next;
+				delete temp;
+				return;
             }
-            nuevo->prev = tail;
-            tail-> next = nuevo;
-            tail = nuevo;
-	    }
-		
-		void insert(int pos, Nodo<T> *nuevo){
+            Nodo<T> *it=root;
+
+            while (it->next!=NULL && it->next->value != it->value){    
+                it=it->next;
+            }
+
+            if(it->next!=NULL){
+                Nodo<T> *temp=it;
+				it = temp->next;
+				delete temp;
+			}
+
+        }*/
+
+        void SortedInsert (T value){
+			Nodo <T> *nuevo = new Nodo <T>(value);
+			Nodo <T> *it = root;
+
+			while(nuevo->value > it->value && it->next != NULL){
+				it = it->next;
+			}
+
+			if(nuevo->value <= it->value){
+				nuevo->next = it->next;
+				it->next = nuevo;
+			}
+
+			if(it->value < nuevo->value){
+				it->next = value;
+			}
+        }
+
+        /*void insert(int pos, Nodo<T> *nuevo){
 			if(root==NULL){
 				return;
 			}
 			int actual=0;
 			Nodo<T> *it=root;
-			if(pos==0){
+			/*if(pos==0){
 				nuevo->next=root;
 				root=nuevo;
 				return;
@@ -119,140 +146,85 @@ class LinkedList{
 				nuevo->next=it->next;
 				it->next=nuevo;
 			}
-		}
-        void insert(int pos, T value){
-            Nodo<T> *nuevo=new Nodo<T>(value);
-            insert(pos, nuevo);
-	    }
+		}*/
 
-        void DeleteList(){
-            Nodo<T> *it=head;
-			while(it->next != NULL){
-
-				if(it->prev!=NULL){
-                    it->prev->next=it->next;
-                }
-                else{
-                    head=it->next;
-                }
-
-                if(it->next!=NULL){
-                    it->next->prev=it->prev;
-                }
-                else{
-                    tail=it->prev;
-                }
-                delete it;
-		    }
-			
-        }
-		void insert(int pos, Nodo<T> *nuevo){
-
-            if(head==NULL){
-                    append(nuevo);
-                    return;
-            }
-            if (pos==0){
-                nuevo->next=head;
-                head->prev=nuevo;
-                head=nuevo;
-                return;
-            }
-            int p=0;
-            Nodo<T> *it=head;
-            while(it->next!=NULL && p<pos){
-                it=it->next;
-                p++;
-            }
-            if(p==pos){
-                it->prev->next=nuevo;
-                nuevo->prev=it->prev;
-                it->prev=nuevo;
-                nuevo->next=it;
-            }
-            if((p+1)==pos){
-                append(nuevo);
-            }
-            
-    	}
-
-		void remover(T value){
-            Nodo<T> *it=head;
-            while(it!=NULL && it->value!=value){
-                it=it->next;
-                }
-            if(it!=NULL){
-                if(it->prev!=NULL){
-                    it->prev->next=it->next;
-                }else{
-                    head=it->next;
-                }
-                if(it->next!=NULL){
-                    it->next->prev=it->prev;
-                }else{
-                    tail=it->prev;
-                }
-                delete it;
-            }
-	    }
-
-		void insert(int pos, T val){
-			Nodo<T> *nuevo=new Nodo<T>(val);
-			this->insert(pos, nuevo);
-		}
-		
-		void eliminar(T val){
-			if(root==NULL){
+		 void removeDuplicates(){
+            Nodo <T> *it=root;
+			Nodo <T> *temp=root;
+			if(root == NULL){
 				return;
 			}
-			if(root->value==val){
-				Nodo<T> *temp=root;
+
+            if(root->value == root->next->value){
+                Nodo<T> *temp=root;
 				root=root->next;
 				delete temp;
-				return;
+				return; 
+            }
+            
+            while(it->next != NULL){
+                
+                if (it ->value == it ->next->value){ 
+                    Nodo <T> *temp = it;
+                    it = it ->next;
+                    delete temp;
+                    return;                    
+                }
+                it = it->next;
 			}
-			Nodo<T> *it=root;
-			while(it->next!=NULL &&it->next->value!=val){
-				it=it->next;
-			}
-			if(it->next!=NULL){
-				Nodo<T> *temp=it->next;
-				it->next=temp->next;
-				delete temp;
-			}
+            
+			/*
+			
+			
+            
+
+			while(it->next != NULL){
+				while(it != NULL){
+					if(it == it->next){
+						it->next = temp->next;
+						delete temp;
+						temp=it->next;
+					}
+
+					else{
+						it = temp;
+						temp = temp->next;
+					}
+				}
+			}*/
+			//root = root->next;
 		}
-        
-        void imprimirReversa(){
-		Nodo<T> *temp=root;
-		while(temp!=NULL){
-			cout<<temp->value<<",";
-			temp=temp->prev;
-		}
-		cout<<endl;
-	}
+
 
 };
-	
-	
-
 
 int main(){
 	LinkedList<int> ll;
-    int nuevonum = 0;
+	ll.append(5);
+	ll.append(5);
+	ll.append(5);
+	ll.append(7);
 	ll.append(8);
-	ll.append(4);
-	ll.append(6);
-	ll.append(4);
-	ll.append(11);
-    ll.append(5);
-	ll.append(15);
-    ll.append(5);
+    ll.append(9);
+	ll.append(10);
+	ll.imprimir();
+	ll.Count(5);
+    ll.DeleteList();
     ll.imprimir();
-    ll.Count(6);
-    //ll.DeleteList();
-    cout << "Inserte un número para agregar a la lista: "; cin>> nuevonum; cout << endl;
-    ll.insert(3,30);
+	ll.append(5);
+	ll.append(5);
+	ll.append(5);
+	ll.append(7);
+	ll.append(8);
+    ll.append(9);
+	ll.append(10);
+    ll.removeDuplicates();
     ll.imprimir();
-    
-	return 0;
+
+	ll.SortedInsert(6);
+	ll.imprimir();
+    /*ll.append(1);
+    ll.append(2);
+    ll.append(4);
+	return 0;*/
 }
